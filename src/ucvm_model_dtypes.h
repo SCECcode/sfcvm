d4fdfaf332f396ed48572235f36de93d43fb51b1
Mei-Hui Su <mei@Mei-Huis-MacBook-Pro.local>
2024-01-03 21:09:23 -0800

    wip

--- src/ucvm_model_dtypes.h
+++ src/ucvm_model_dtypes.h
@@ -23,21 +23,22 @@ for sfcvm,
    { "SQUASH_MIN_ELEV" : -5000.0 }  
 */
 
-typedef enum { UCVM_MODEL_PARAM_CONF_BLOB = 0,
+typedef enum { 
+               UCVM_MODEL_PARAM_FORCE_DEPTH_ABOVE_SURF = 0,
                UCVM_MODEL_PARAM_QUERY_MODE = 1,
                UCVM_MODEL_PARAM_IFUNC_ZRANGE = 2,
                UCVM_MODEL_PARAM_MODEL_CONF = 3,
-               UCVM_MODEL_PARAM_FORCE_DEPTH_ABOVE_SURF = 4,
+
+               UCVM_MODEL_PARAM_CONF_BLOB = 4,
                UCVM_MODEL_PARAM_PLUGIN_MODE = 5 } ucvm_model_param_t;
 
 /* Supported model parameters. Used internally by UCVM
 UCVM_MODEL_PARAM_FORCE_DEPTH_ABOVE_SURF : Force elevation points to be
-  treated as offset relative to regional model surface when it is
-  above the ucvm dem surface. Removes discontinuities between model
-  surface and GTL.
+          treated as offset relative to regional model surface when it is
+          above the ucvm dem surface. Removes discontinuities between model
+          surface and GTL.
 UCVM_MODEL_PARAM_PLUGIN_MODE : Tell the backend model that it is being
-  invoked as plugin module and is always expected to be 'query by depth'
-
+          invoked as plugin module and is always expected to be 'query by depth'
 */
 
 /* Supported coordinate query modes */
